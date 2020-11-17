9e7727b6d48bf88391031f96bb83f5a69b9df1ad
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-17 10:29:54 -0600

    Validaciones dentro de la lógica del negocio

--- elephant-carpaccio/factura.rb
+++ elephant-carpaccio/factura.rb
@@ -4,21 +4,31 @@ require_relative "./models/tax.model.rb"
 if (ARGV.length != 3)
     raise "Debe tener 3 parametros de entrada: Unidades (entera), Costo (Flotante) y estado (Cadena)"
 end
-unit = 0;
+
+units = 0;
 begin
-    unit = Integer(ARGV[0]);
+    units = Integer(ARGV[0]);
 rescue
     raise "El primer parametro (unidades) debe ser entero";
 end
+if(units <= 0)
+    raise "El parametro de unidades debe ser mayor a cero"
+end
+
 cost =  0.0
 begin
     cost = Float(ARGV[1]);
 rescue
     raise "El segundo parametro (costo) debe ser flotante";
 end
+if(cost <= 0)
+    raise "El parametro de costo debe ser mayor a cero"
+end
+
 state = ARGV[2].to_s.upcase;
 Tax.exists_state?(state);
-total = Invoice.get_total(unit, cost)
+
+total = Invoice.get_total(units, cost)
 puts "total before discounts #{total}";
 total = Discount.get_discount(total)
 puts "total after discounts #{total}";
