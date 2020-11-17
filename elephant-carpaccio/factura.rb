cb0e7f254d5c9cde7acc1160294825932e622ad4
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-17 09:06:00 -0600

    Se agregó la tabla de descuentos estáticos y así mismo se implementó la función que aplica el descuento respecto al total

--- elephant-carpaccio/factura.rb
+++ elephant-carpaccio/factura.rb
@@ -1,6 +1,8 @@
 require_relative "./models/invoice.model.rb"
+require_relative "./models/discount.model.rb"
 unit = ARGV[0].to_i;
 cost =  ARGV[1].to_f;
 invoice = Invoice.new();
-tmp = invoice.get_total(unit, cost);
-puts "total is #{tmp}"
+total = invoice.get_total(unit, cost);
+total = Discount.get_discount(total);
+puts "total with discount #{total}"
--- elephant-carpaccio/models/discount.model.rb
+++ elephant-carpaccio/models/discount.model.rb
@@ -1,2 +1,47 @@
 class Discount
+    @@discounts = [
+        {
+            id:0,
+            min:0.0,
+            max:1000.0,
+            reduce: 0.0
+        },
+        {
+            id:1,
+            min:1000.0,
+            max:5000.0,
+            reduce: 0.03
+        },
+        {
+            id:2,
+            min:5000.0,
+            max:7000.0,
+            reduce: 0.05
+        },
+        {
+            id:3,
+            min:7000.0,
+            max:10000.0,
+            reduce: 0.07
+        },
+        {
+            id:4,
+            min:10000.0,
+            max:50000.0,
+            reduce: 0.10
+        },
+        {
+            id:5,
+            min:50000.0,
+            max:Float::INFINITY,
+            reduce: 0.15
+        }
+    ];
+
+    def self.get_discount(total)
+        reduce = @@discounts.find { |i| 
+            total >= i[:min] and
+            total < i[:max]}[:reduce]
+        return total * (1-reduce);
+    end
 end
